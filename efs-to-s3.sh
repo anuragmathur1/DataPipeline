278c597f17841e75cc14c63ff6a5df47a2226343
Anurag Mathur <anuragmathur1@aol.com>
2018-01-10 09:43:19 +1100

    Update efs-to-s3.sh

--- efs-to-s3.sh
+++ efs-to-s3.sh
@@ -15,30 +15,22 @@
 
 # Input arguments
 source=$1
-destination=$2
-interval=$3
-retain=$4
-efsid=$5
-S3DestinationPath=$6
+efsid=$2
+S3DestinationPath=$3
 
 # Prepare system for rsync
-#echo 'sudo yum -y update'
-#sudo yum -y update
 echo 'sudo yum -y install nfs-utils'
 sudo yum -y install nfs-utils
 echo 'sudo mkdir /backup'
 sudo mkdir /backup
 
 echo "sudo mount -t nfs -o nfsvers=4.1 -o rsize=1048576 -o wsize=1048576 -o timeo=600 -o retrans=2 -o hard $source /backup"
-#sudo mount -t nfs -o nfsvers=4.1 -o rsize=1048576 -o wsize=1048576 -o timeo=600 -o retrans=2 -o hard $source /backup
-sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 fs-3552a70c.efs.ap-southeast-2.amazonaws.com:/ /backup
+sudo mount -t nfs -o nfsvers=4.1 -o rsize=1048576 -o wsize=1048576 -o timeo=600 -o retrans=2 -o hard $source /backup
 
 echo "sudo aws s3 sync /backup $S3DestinationPath"
-#sudo aws s3 sync /backup $S3DestinationPath
-sudo aws s3 sync /backup s3://data-pipeline-am/backup-1
+sudo aws s3 sync /backup $S3DestinationPath
 syncStatus=$?
-
-echo "sudo aws s3 cp /tmp/efs-backup.log s3://data-pipeline-am/logs/$efsid-`date +%Y%m%d-%H%M`.log"
-sudo aws s3 mv /tmp/efs-backup.log s3://data-pipeline-am/logs/$efsid-`date +%Y%m%d-%H%M`.log
+echo "sudo aws s3 cp /tmp/efs-backup.log s3://datapipeline.elmodev.com/logs/$efsid-`date +%Y%m%d-%H%M`.log"
+sudo aws s3 mv /tmp/efs-backup.log s3://datapipeline.elmodev.com/logs/$efsid-`date +%Y%m%d-%H%M`.log
 
 exit $syncStatus
